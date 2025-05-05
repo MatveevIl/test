e77afbbc260891ef8e4042ab59ec3f13df265322
MatveevIl <ilmatveev05@gmail.com>
2025-05-05 22:09:23 +0300

    Добавлены комментарии к коду

--- test.cpp
+++ test.cpp
@@ -3,10 +3,10 @@
 using namespace cv;
 using namespace std;
 
-void blurFace(Mat& image, Rect face) {
-    Mat faceROI = image(face);
-    GaussianBlur(faceROI, faceROI, Size(101, 101), 0); 
-    faceROI.copyTo(image(face));
+void blurFace(Mat& image, Rect face) { //Mat - объект(матрица), представляющий изображение; объект Rect - прямоугольник лица
+    Mat faceROI = image(face); //создаёт объект, типа Mat, являющийся регионом интереса
+    GaussianBlur(faceROI, faceROI, Size(101, 101), 0); //фэйсРои - лицо входящее, фэйсРОИ - лицо выходящее; размер ядра Гаусса и отклонение по Х и У - 0(стандартное)
+    faceROI.copyTo(image(face)); //копируем размытое на исходное изображение
 }
 
 int main() {
@@ -16,25 +16,31 @@ int main() {
     cout << "Введите имя файла изображения: ";
     cin >> image_filename;
 
-    Mat image = imread(image_filename);
+    Mat image = imread(image_filename); // создаём объект имаге типа Мат, имрид - функция опенсв для чтения изображений 
 
     if (image.empty()) {
         cout << "Не удалось загрузить изображение: " << image_filename << endl;
         return -1;
     }
 
-    CascadeClassifier face_cascade;
+    CascadeClassifier face_cascade; //объявляем объект стандартного для опенСВ класса CascadeClassifier для обнаружения объектов
     if (!face_cascade.load("D:\\openCV\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml")) {
         cout << "Не удалось загрузить классификатор каскадов Хаара" << endl;
         return -1;
     }
 
-    std::vector<Rect> faces;
-    face_cascade.detectMultiScale(image, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+    std::vector<Rect> faces; //объявляем faces = вектор(массив, изменяющийся динамически)  для хранения прямоугольников = Rect
+    face_cascade.detectMultiScale(image, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30)); 
+    /*функция для обнаружения лиц(входное изображение; вектор, куда сохраняем найденные лица;
+    1.1 - параметр масштабирования, чем меньше - тем точнее, но дольше работает; 
+    2 - параметр, определяющий, сколько прямоугольников дб рядом, чтоб кандидат стал лицом, чем больше значение - тем меньше ложных срабатываний, но можно пропустить лица
+    0 - других флагов нет
+    CASCADE_SCALE_IMAGE - размер изображения масштабируем во время поиска
+    Сайз - минимальный размер лица для определения*/
 
     for (size_t i = 0; i < faces.size(); i++) {
         blurFace(image, faces[i]);
-    }
+    }//Для каждого лица вызываем функцию размывания лиц
 
     string output_filename = "blurred_" + image_filename;
 
